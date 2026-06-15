017dbc7652ca156fb310720327c6969d0706fc37
Keshav Mundel <keshavmundel076@gmail.com>
2026-06-15 10:58:30 +0530

    authentication system

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,3 +1,4 @@
+from flask import request
 import flask
 
 #Blueprint is a Flask feature used to organize routes.
@@ -7,10 +8,41 @@ import flask
 
 auth = flask.Blueprint("auth", __name__)
 
-@auth.route("/login") #This is called a decorator. A decorator modifies a function.
+#This is called a decorator. A decorator modifies a function.
+@auth.route("/login", methods = ["POST"])
 def login():
-    return "login successfull"
 
-@auth.route("/register")
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+    
+    return f"email: {email}, password: {password}"
+
+@auth.route("/register", methods = ["POST"])
 def register():
-    return "user registered successfully"
+    data = request.get_json()
+
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+    institution = data.get("institution")
+
+    if not email:
+        return {"error":"email is required"}
+    if '@' not in email:
+        return {"error": "@ is required"}
+    
+    if not name:
+        return {"error" : "name is required"}
+    
+    if not password or len(password) < 6:
+        return {"error" : "enter password greater than 6 digits"}
+    
+
+    return {
+        "name": name,
+        "email": email,
+        "password": password,
+        "institution": institution
+    }
