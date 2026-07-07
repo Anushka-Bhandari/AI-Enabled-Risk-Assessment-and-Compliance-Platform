5e694cf0d7a7ecef4552a09e01f2466b561fd699
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-07-07 18:20:20 +0530

    Add document assessment models and migrations

---
+++ backend/migrations/versions/3c9ec25b9e6e_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 3c9ec25b9e6e
+Revises: 7a1866addc9c, d2b2890dafeb
+Create Date: 2026-07-07 17:58:24.828364
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3c9ec25b9e6e'
+down_revision = ('7a1866addc9c', 'd2b2890dafeb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
